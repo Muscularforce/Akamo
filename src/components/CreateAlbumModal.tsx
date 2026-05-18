1173342bc7d356f4435ff07643f63c6547c8999d
Muscularforce <jovanf.fernandes@gmail.com>
2026-05-18 23:35:05 +0530

    Remove cover art hover overlay completely to fix layout issue

--- src/components/CreateAlbumModal.tsx
+++ src/components/CreateAlbumModal.tsx
@@ -227,10 +227,7 @@ export default function CreateAlbumModal({ isOpen, onClose, onCreate, tracks }: 
                           )}
                         </div>
                       )}
-                      {/* Hover overlay */}
-                      <div className="absolute inset-0 bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity flex items-center justify-center rounded-2xl">
-                        <Upload size={20} className="text-white" />
-                      </div>
+
                       <input
                         ref={coverInputRef}
                         type="file"
